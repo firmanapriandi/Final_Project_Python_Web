d8030ff16fb3e49a83742c181a958707fc4e5ee8
firmanapriandi <firmanapriandi01@gmail.com>
2022-09-30 16:05:35 +0700

    Add path, and modified all templates

--- app.py
+++ app.py
@@ -10,5 +10,45 @@ def index():
         f.close()
     return render_template("index.html", fileData = fileData)
 
+@app.route("/about", methods=["POST", "GET"])
+def about():
+    return render_template("about.html")
+
+@app.route("/blog", methods=["POST", "GET"])
+def blog():
+    return render_template("blog.html")
+
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    return render_template("contact.html")
+
+@app.route("/detail", methods=["POST", "GET"])
+def detail():
+    return render_template("detail.html")
+
+@app.route("/feature", methods=["POST", "GET"])
+def feature():
+    return render_template("feature.html")
+
+@app.route("/price", methods=["POST", "GET"])
+def price():
+    return render_template("price.html")
+
+@app.route("/quote", methods=["POST", "GET"])
+def quote():
+    return render_template("quote.html")
+
+@app.route("/service", methods=["POST", "GET"])
+def service():
+    return render_template("service.html")
+
+@app.route("/team", methods=["POST", "GET"])
+def team():
+    return render_template("team.html")
+
+@app.route("/testimoni", methods=["POST", "GET"])
+def testimoni():
+    return render_template("testimonial.html")
+
 if __name__ == "__main__":
   app.run()
